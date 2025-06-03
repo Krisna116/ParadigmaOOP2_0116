ad47aa1000742ffdb01b74c20137e5cf9057c176
Krisna <your-email@example.com>
2025-06-03 18:10:45 +0700

    membuat int main

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -6,13 +6,12 @@ class seseorang {
     virtual void pesan() = 0;
     // virtual void pesn(){
     // cout << "pesan dari seseorang endl;"
-    //
     // }
 
 };
 class joko : public seseorang{
     public: 
-    void pesam() {
+    void pesan() {
         cout << "Pesan dari joko" << endl;
     }
 };
@@ -22,3 +21,18 @@ class lia: public seseorang {
         cout << "Pesan dari lia" << endl;
     }
 };
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = & a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan ();
+    a.seseorang ::pesan();
+    b.seseorang ::pesan();
+
+    return 0;
+
+}
