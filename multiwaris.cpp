5409fcbf0b23d92173c0fb1564f1da234695d01d
Krisna <your-email@example.com>
2025-06-03 20:17:53 +0700

    class orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -9,34 +9,3 @@ class orang {
          cout << "orang dibuat dengan umur" << umur << "\n" << endl;
         }    
 };
-
-class pekerja : virtual public orang {
-    public :
-    pekerja(int pUmur) :
-        orang(pUmur)
-        {
-            cout << "pekerja dibuat\n" << endl;
-        }
-
-};
-class pelajar : virtual public orang {
-    public :
-    int umur;
-    pelajar(int pUmur):
-         orang(pUmur){
-            cout << "Pelajar dibuat\n" << endl;
-         }
-};
-class budi : public pekerja, public pelajar {
-    public:
-    budi (int pUmur):
-    orang(pUmur),
-    pelajar(pUmur),
-    pekerja(pUmur){
-        cout << "Budi dibuat\n" << endl;
-    }
-};
-int main() {
-    budi a(12);
-    return 0;
-}
